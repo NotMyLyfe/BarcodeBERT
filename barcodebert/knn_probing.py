50cf5ae4a28db8dca881336e7bc1ae05fb1bd6f0
Gordon Lin <contact@gordonlin.ca>
2025-03-15 19:02:59 -0400

    Modified representations_from_df in BarcodeBERT to use training labels

--- barcodebert/knn_probing.py
+++ barcodebert/knn_probing.py
@@ -145,10 +145,10 @@ def run(config):
     # Generate emebddings for the training and test sets
     print("Generating embeddings for test set", flush=True)
     X_unseen, y_unseen, orders = representations_from_df(
-        df_test, config.target_level, model, tokenizer, config.dataset_name
+        df_test, df_train, config.target_level, model, tokenizer, config.dataset_name
     )
     print("Generating embeddings for train set", flush=True)
-    X, y, train_orders = representations_from_df(df_train, config.target_level, model, tokenizer, config.dataset_name)
+    X, y, train_orders = representations_from_df(df_train, df_train, config.target_level, model, tokenizer, config.dataset_name)
     timing_stats["embed"] = time.time() - t_start_embed
 
     c = 0
